5c6a325e0c3c8d353b2d5c7ad490c47b6396c29d
hades <43458821+drhades007@users.noreply.github.com>
2018-09-21 10:27:58 +0700

    Add files via upload

---
+++ bil1&bil2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+//dr.hades007
+//https://github.com/drhades007
+main() {
+
+	int a,b,hasil;
+	
+	printf("masukkan bilangan \n");
+	printf("bil 1	:");
+	scanf("%d",&a);//menginputkan suatu alamat yang dituju
+	printf("bil 2	:");
+	scanf("%d",&b);//menginputkan suatu alamat yang dituju
+	hasil=a+b;//menambahkan bilangan 1 dan bilangan ke dua dengan operator aritmatika pertambahan
+	printf("hasil :",hasil);//mengoutputkan hasil dari perjumlahan tersebut
+}
